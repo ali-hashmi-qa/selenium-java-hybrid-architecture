f76a7df68e0f5c837426315c542d21996f93b0b5
Ali Hashmi <hashmiali2000@gmail.com>
2026-07-11 16:47:01 +0500

    Implement core framework setup with DriverFactory, ConfigReader and BaseTest

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -1,5 +1,49 @@
 package factory;
 
-public class DriverFactory {
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ConfigReader;
 
+public class DriverFactory {
+	
+	public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	
+	public static void initDriver() {
+		String browser = ConfigReader.get("browser");
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver.set(new ChromeDriver());
+		}
+		else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver.set(new FirefoxDriver());
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver.set(new EdgeDriver());
+
+        } else {
+            throw new RuntimeException("Unsupported browser: " + browser);
+        }
+		getDriver().manage().window().maximize();
+		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(
+				Integer.parseInt(ConfigReader.get("timeout"))));
+		
+		getDriver().get(ConfigReader.get("url"));
+	}
+	
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
+	
+	public static void quitDriver() {
+		if(getDriver()!=null) {
+		getDriver().quit();
+		driver.remove();
+		}
+	}
 }

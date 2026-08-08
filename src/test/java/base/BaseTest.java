2e4b3e266db5d1bb8c4e9e17af7e9839a15c5455
ali-hashmi-qa <hashmiali2000@gmail.com>
2026-08-08 14:40:02 +0500

    Integrated enterprise-grade Extent reporting, screenshot capture and Log4j2 logging with dynamic report generation

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,19 +1,22 @@
 package base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-
 import factory.DriverFactory;
 
+
 public class BaseTest {
-	
+	private static final Logger logger = LogManager.getLogger(BaseTest.class);	
 	protected WebDriver driver;
 	
 	@BeforeMethod
 	@Parameters("browser")
 	public void setup(String browser) {
+		logger.info("Starting browser: {}", browser);
 		DriverFactory.initDriver(browser);
 		driver = DriverFactory.getDriver();
 	}
--- src/test/java/constants/FrameworkConstants.java
+++ src/test/java/constants/FrameworkConstants.java
@@ -2,7 +2,21 @@ package constants;
 
 public class FrameworkConstants {
 	
-	public static final String Config_File_Path = System.getProperty("user.dir") + 
+	public static final String User_Dir = System.getProperty("user.dir");
+	
+	public static final String Config_File_Path = User_Dir + 
 			"\\src\\test\\resources\\config\\config.properties";
-
+	
+	public static final String Test_Output_Folder = User_Dir + "\\test-output\\";
+	
+	public static final String Extent_Report_Folder = Test_Output_Folder + "extent-reports\\";
+	
+	public static final String Extent_Report_Name = "extent-report";
+	
+	public static final String Report_Extension = ".html";
+	
+	public static final String Screenshots_Folder = Test_Output_Folder + "screenshots\\";
+	
+	public static final String Logs_Folder = Test_Output_Folder + "logs\\";
+	
 }

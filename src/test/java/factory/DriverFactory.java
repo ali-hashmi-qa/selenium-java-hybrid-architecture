b88c6ab8181d2b62df8c349526eb691867a6a1b3
Ali Hashmi <hashmiali2000@gmail.com>
2026-08-19 22:01:27 +0500

    Integrated GitHub Actions pipeline and enable cross-platform OS path handling

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -3,8 +3,11 @@ package factory;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.ConfigReader;
 
@@ -17,12 +20,26 @@ public class DriverFactory {
 		if(browser==null) {
 		 browser = ConfigReader.get("browser");
 		}
+		
+		boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", ConfigReader.get("headless")));
 		if(browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
+			ChromeOptions chromeOptions = new ChromeOptions();
+			if (isHeadless) {
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                }
 			driver.set(new ChromeDriver());
 		}
 		else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            if (isHeadless) {
+                firefoxOptions.addArguments("-headless");
+                firefoxOptions.addArguments("--window-size=1920,1080");
+            }
             driver.set(new FirefoxDriver());
 
         } else if (browser.equalsIgnoreCase("edge")) {

a5162c85b52903f464a9b2ac3b19caf849473cd9
ali-hashmi-qa <hashmiali2000@gmail.com>
2026-08-24 21:48:40 +0500

    Implemented containerized Selenium Grid infrastructure with Docker Compose and GitHub Actions CI pipeline

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -1,12 +1,14 @@
 package factory;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.ConfigReader;
@@ -22,39 +24,69 @@ public class DriverFactory {
 		}
 		
 		boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", ConfigReader.get("headless")));
-		if(browser.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions chromeOptions = new ChromeOptions();
-			if (isHeadless) {
-                chromeOptions.addArguments("--headless=new");
-                chromeOptions.addArguments("--no-sandbox");
-                chromeOptions.addArguments("--disable-dev-shm-usage");
-                chromeOptions.addArguments("--window-size=1920,1080");
-                }
-			driver.set(new ChromeDriver(chromeOptions));
+		String executionEnv = System.getProperty("execution_env", ConfigReader.get("execution_env"));
+		if (executionEnv.equalsIgnoreCase("grid")) {
+			initRemoteDriver(browser, isHeadless);
 		}
-		else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions firefoxOptions = new FirefoxOptions();
-            if (isHeadless) {
-                firefoxOptions.addArguments("-headless");
-                firefoxOptions.addArguments("--window-size=1920,1080");
-            }
-            driver.set(new FirefoxDriver(firefoxOptions));
-
-        } else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            driver.set(new EdgeDriver());
-
-        } else {
-            throw new RuntimeException("Unsupported browser: " + browser);
-        }
-		getDriver().manage().window().maximize();
+		else {
+			initLocalDriver(browser, isHeadless);
+		}
 		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(
 				Integer.parseInt(ConfigReader.get("timeout"))));
 		
 		getDriver().get(ConfigReader.get("url"));
 	}
+	
+	private static void initLocalDriver(String browser, boolean isHeadless) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            if (isHeadless) {
+                chromeOptions.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080");
+            }
+            driver.set(new ChromeDriver(chromeOptions));
+		}
+            else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+             if (isHeadless) {
+                 firefoxOptions.addArguments("-headless", "--window-size=1920,1080");
+                }
+             driver.set(new FirefoxDriver(firefoxOptions));
+            }
+            else {
+            	throw new RuntimeException("Unsupported browser: " + browser);
+            }
+	}
+		
+	private static void initRemoteDriver(String browser, boolean isHeadless) {
+		String gridUrl = ConfigReader.get("grid_url");
+		if (browser.equalsIgnoreCase("chrome")) {
+	        ChromeOptions chromeOptions = new ChromeOptions();
+	         if (isHeadless) {
+	             chromeOptions.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080");
+	         }
+	         try {
+				driver.set(new RemoteWebDriver(new URL(gridUrl), chromeOptions));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+	         else if (browser.equalsIgnoreCase("firefox")) {
+	            FirefoxOptions firefoxOptions = new FirefoxOptions();
+	             if (isHeadless) {
+	                 firefoxOptions.addArguments("-headless", "--window-size=1920,1080");
+	                }
+	             try {
+					driver.set(new RemoteWebDriver(new URL(gridUrl), firefoxOptions));
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+	            }
+	            else {
+	            	throw new RuntimeException("Unsupported browser: " + browser);
+	            }
+		}
 	
 	public static WebDriver getDriver() {
 		return driver.get();
